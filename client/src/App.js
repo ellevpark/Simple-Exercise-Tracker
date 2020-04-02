2d7c7b33df18ef377bc8f3fb5c799561ceb6cf21
Ellen Park <ellenpark781@gmail.com>
2020-04-02 10:49:59 -0400

    edited folders

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter as Router, Route} from "react-router-dom";
 
 import Navbar from "./components/Navbar"
-import ExercisesList from "./components/Home";
+import Home from "./components/Home";
 import EditExercise from "./components/EditExercise";
 import CreateExercise from "./components/CreateExercise";
 import CreateUser from "./components/CreateUser";
@@ -14,7 +14,7 @@ function App() {
       <div className="container">
       <Navbar />
       <br/>
-      <Route path="/" exact component={ExercisesList} />
+      <Route path="/" exact component={Home} />
       <Route path="/edit/:id" component={EditExercise} />
       <Route path="/create" component={CreateExercise} />
       <Route path="/user" component={CreateUser} />
